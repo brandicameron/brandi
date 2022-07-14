360b387cbd2cd5d0eaf6681591273c9c30871094
Brandi Cameron <hello@brandicameron.com>
2022-07-14 10:05:32 -0500

    Changed project view on ipad to flex column

--- components/home/Quote.js
+++ components/home/Quote.js
@@ -10,7 +10,7 @@ export default function Quote() {
           variants={rush}
           initial='initial'
           whileInView={'animate'}
-          // viewport={{ once: true }}
+          viewport={{ once: true }}
           aria-label='rush'
           className={styles.rush}
           src='/images/home/rush.svg'
@@ -48,32 +48,3 @@ const rush = {
     },
   },
 };
-// const rush = {
-//   initial: {
-//     skewX: 75,
-//     x: '-50vw',
-//   },
-//   animate: {
-//     skewX: 0,
-//     x: 0,
-//     transition: {
-//       type: 'spring',
-//       stiffness: 100,
-//       duration: 0.75,
-//       bounce: 2,
-//     },
-//   },
-// };
-
-// const free = {
-//   initial: {
-//     scale: 0,
-//   },
-//   animate: {
-//     scale: 1,
-//     transition: {
-//       duration: 0.25,
-//       delay: 0.2,
-//     },
-//   },
-// };
