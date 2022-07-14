d16536a505da216d5e2431c222bafcb744399e9f
Brandi Cameron <hello@brandicameron.com>
2022-07-14 10:58:17 -0500

    Added animation to cta link

--- components/home/CTA.js
+++ components/home/CTA.js
@@ -1,6 +1,7 @@
 import styles from '../../styles/home/CTA.module.css';
 import Link from 'next/link';
 import StripeBorder from '../shared/StripeBorder';
+import { motion } from 'framer-motion';
 
 export default function CTA() {
   return (
@@ -10,10 +11,30 @@ export default function CTA() {
         <span className={styles.heyText} aria-hidden></span>
         visit my code{' '}
         <Link href='/playground'>
-          <a>Playground </a>
+          <a>
+            Playground{' '}
+            <motion.span
+              className={styles.underline}
+              variants={link}
+              initial='initial'
+              whileInView={'animate'}
+            ></motion.span>{' '}
+          </a>
         </Link>
         to see more fun projects!
       </h2>
     </section>
   );
 }
+
+const link = {
+  initial: {
+    scaleX: 0,
+  },
+  animate: {
+    scaleX: 1,
+    transition: {
+      delay: 0.25,
+    },
+  },
+};
